3f2953a7fdfa8e23a252f0ba82eec1075c18c4ed
Trần Đình Mạnh Cường <81034438+cuongtran2203@users.noreply.github.com>
2024-06-15 18:06:58 +0700

    change daataset

--- AI_tools/train.py
+++ AI_tools/train.py
@@ -73,7 +73,7 @@ def plot_diagram(e,data1,data2,label1,label2,label3):
 if __name__ == '__main__':
     data = []
     dev = torch.device("cpu")
-    train_dataset, val_dataset, test_dataset = load_data(csv_file='Dataset/test.csv')
+    train_dataset, val_dataset, test_dataset = load_data(csv_file='Dataset/data_1714496400000_1717174800000.csv')
     train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=1)
     val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=1)
     test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=1)
